e83b78d6eec8cf0a9a7c7cca494b0967f0457244
FrankAcevedo <114449752+FrankAcevedo@users.noreply.github.com>
2023-06-26 22:51:28 -0500

    SPRINT BACKLOG 1 - FRANK ACEVEDO
    Terminado Usuario - Rol - Rutina

---
+++ RunnersUnitedBackend/src/main/java/com/upao/runnersunited/domain/Rutina.java
@@ -0,0 +1,96 @@
+package com.upao.runnersunited.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+@Entity
+@Table(name = "rutinas")
+public class Rutina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private String nivel;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private int cantidadDias;
+
+    public Rutina() {
+        this.fechaInicio = LocalDate.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.fechaFin = LocalDate.parse(fechaFin, formatter);
+        calcularCantidadDias();
+    }
+
+    public int getCantidadDias() {
+        return cantidadDias;
+    }
+
+    private void calcularCantidadDias() {
+        cantidadDias = (int) ChronoUnit.DAYS.between(fechaInicio, fechaFin);
+    }
+}
---
+++ RunnersUnitedBackend/src/main/java/com/upao/runnersunited/service/RutinaServiceImpl.java
@@ -0,0 +1,54 @@
+package com.upao.runnersunited.service;
+
+import com.upao.runnersunited.domain.Rutina;
+import com.upao.runnersunited.repository.RutinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RutinaServiceImpl implements RutinaService {
+
+    @Autowired
+    private RutinaRepository rutinaRepository;
+
+    @Override
+    public List<Rutina> obtenerTodasLasRutinas() {
+        return rutinaRepository.findAll();
+    }
+
+    @Override
+    public Rutina obtenerRutinaPorId(Long id) {
+        return rutinaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rutina no encontrada"));
+    }
+
+    @Override
+    public Rutina crearRutina(Rutina rutina) {
+        return rutinaRepository.save(rutina);
+    }
+
+    @Override
+    public Rutina actualizarRutina(Long id, Rutina rutinaActualizada) {
+        Rutina rutina = rutinaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rutina no encontrada"));
+
+        rutina.setNombre(rutinaActualizada.getNombre());
+        rutina.setDescripcion(rutinaActualizada.getDescripcion());
+        rutina.setNivel(rutinaActualizada.getNivel());
+
+        return rutinaRepository.save(rutina);
+    }
+
+    @Override
+    public void eliminarRutina(Long id) {
+        Rutina rutina = rutinaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rutina no encontrada"));
+
+        rutinaRepository.delete(rutina);
+    }
+}
+
+
+
